bf4837066609fd2b8c5965c0032436dfbd5b16e9
Ash Matheson <ash.matheson@gmail.com>
2019-08-31 20:29:15 -0700

    Initial commit

---
+++ Viewer/Viewer/Viewer.cpp
@@ -0,0 +1,31 @@
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
+
+#include <iostream>
+
+int main()
+{
+	std::string pFile("D:\\dev\\modelviewer\\Viewer\\assets\\ss\\SportCar20.FBX");
+
+	// Create an instance of the Importer class
+	Assimp::Importer importer;
+
+	// And have it read the given file with some example postprocessing
+	// Usually - if speed is not the most important aspect for you - you'll
+	// probably to request more postprocessing than we do in this example.
+	const aiScene* scene = importer.ReadFile(pFile,
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices |
+		aiProcess_SortByPType);
+
+	if (!scene)
+	{
+		return -1;
+	}
+
+    std::cout << "Hello World!\n";
+
+	return 0;
+}
